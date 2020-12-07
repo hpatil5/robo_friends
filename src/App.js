60ef159bd0fb9bd557f9ea7f01aecb15e04ef993
HarishMPatil <32559181+HarishMPatil@users.noreply.github.com>
2020-12-07 23:28:53 +0530

    Description : building initial card component and displaying it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import './App.css';
+import "./App.css";
+import Card from "./components/Card";
+import { robots } from "./components/robots";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       <h1>
-         Hello World
-       </h1>
-      </header>
+      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
     </div>
   );
 }
