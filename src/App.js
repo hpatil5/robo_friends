19b6722c3fb17e26514f2148818c40afc715f8a8
HarishMPatil <32559181+HarishMPatil@users.noreply.github.com>
2020-12-09 08:53:28 +0530

    Description : robot card component and fetch users  data from api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
+import React from "react";
 import "./App.css";
-import Card from "./components/Card";
+import CardList from "./components/CardList";
 import { robots } from "./components/robots";
+import SearchBox from "./components/SearchBox";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchField: "",
+    };
+  }
+
+  onSearchChange = (event) => {
+    this.setState({ searchField: event.target.value });
+  };
+
+  componentDidMount() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        return response.data;
+      })
+      .then((users) => this.setState({ robots: users }));
+  }
+
+  render() {
+    const filteredRobots = this.state.robots.filter((robot) =>
+      robot.name.toLowerCase().includes(this.state.searchField.toLowerCase())
+    );
+    if (this.state.robots.length === 0) {
+      return <h1 className="tc">Loading</h1>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
+  }
 }
 
 export default App;
